0746083c2ea998d3741c60f219696730922249dd
Kit Yan Choi <kit@kychoi.org>
2015-09-16 14:04:03 +0100

    Added more test for geodat

--- geodat/test_nc.py
+++ geodat/test_nc.py
@@ -1,3 +1,5 @@
+import os
+
 import unittest
 import numpy
 import functools
@@ -5,22 +7,22 @@ import functools
 import geodat.nc
 
 def var_dummy(ntime,nlat,nlon):
-    time_dim = geodat.nc.Dimension(data=numpy.arange(ntime),
+    time_dim = geodat.nc.Dimension(data=numpy.arange(ntime,dtype=numpy.float),
                                  units="months since 0001-01-01",
                                  dimname="time",
                                  attributes={"calendar":"julian"})
     lon_dim = geodat.nc.Dimension(data=numpy.linspace(0.,360.,nlon,
-                                                    endpoint=False),
-                                units="degreeE",
-                                dimname="lon")
+                                                      endpoint=False),
+                                  units="degreeE",
+                                  dimname="lon")
     lat_dim = geodat.nc.Dimension(data=numpy.linspace(-90.,90.,nlat),
                                 units="degreeN",
                                 dimname="lat")
     
     return geodat.nc.Variable(data=numpy.arange(float(ntime*nlat*nlon)).\
-                            reshape(ntime,nlat,nlon),
-                            dims=[time_dim,lat_dim,lon_dim],
-                            varname="temp")
+                              reshape(ntime,nlat,nlon),
+                              dims=[time_dim,lat_dim,lon_dim],
+                              varname="temp")
 
 
 
@@ -44,7 +46,11 @@ class NCVariableTestCase(unittest.TestCase):
     def setUp(self):
         self.var = var_dummy(12,50,60)
         self.__name__ = "temp"
-    
+
+    def tearDown(self):
+        if os.path.exists("test_nc_file.nc"):
+            os.remove("test_nc_file.nc")
+        
     def test_getvar_data(self):
         self.assertEqual(self.var.data.shape,(12,50,60))
     
@@ -76,8 +82,14 @@ class NCVariableTestCase(unittest.TestCase):
 
     def test_add_broadcast(self):
         self.assertTrue(numpy.allclose((self.var.time_ave() + self.var).data,
-                                       self.var.data.mean(axis=0) + self.var.data))
+                                       self.var.data.mean(axis=0) + \
+                                       self.var.data))
     
+    def test_savefile(self):
+        geodat.nc.savefile("test_nc_file.nc",self.var)
+        tmp = geodat.nc.getvar("test_nc_file.nc","temp")
+        self.assertTrue(numpy.allclose(self.var.data,tmp.data))
+
     @skipUnlessSpharmExists()
     def test_regrid_spharm(self):
         regridded = geodat.nc.regrid(self.var,nlat=100,nlon=120)
@@ -86,9 +98,9 @@ class NCVariableTestCase(unittest.TestCase):
     
     @skipUnlessPyFerretExists()
     def test_regrid_pyferret(self):
-        regridded = geodat.nc.pyferret_regrid(self.var,nlat=100,nlon=120)
+        regridded = geodat.nc.pyferret_regrid(self.var[...,::2,::2], self.var)
         self.assertAlmostEqual(float(regridded.wgt_ave().data),
-                               17999.5,1)
+                               17999.0,1)
 
 
 if __name__== "__main__":
