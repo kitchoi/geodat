73be557a9fa2fec59609d948ef5ad00cb0e3ac7f
Kit Yan Choi <kit@kychoi.org>
2015-09-16 18:15:11 +0100

    Moved docs source to the source directory

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ import alabaster
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinxcontrib.napoleon', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.pngmath', 'sphinx.ext.mathjax', 'sphinx.ext.viewcode','alabaster']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinxcontrib.napoleon','alabaster']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
