ad443debd7b754f4f59476ea7603483a4b99b22a
Kit Yan Choi <kit@kychoi.org>
2015-09-28 17:21:37 +0100

    Travis: simply setup for testing

--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@
 
 from setuptools import setup
 
+with open("requirements.txt") as f:
+    requires = f.readlines()
+
+requires = [ req.replace("\n","") for req in requires ]
+requires = [ req for req in requires if req ]
+
+
 setup(name='Geodat',
       version='1.0',
       description='Geophysical Data Analysis Tool (GeoDAT)',
@@ -9,6 +16,6 @@ setup(name='Geodat',
       author_email='kit@kychoi.org',
       url='http://kychoi.org/geodat_doc/',
       packages=["geodat","geodat.plot"],
-      install_requires = ["numpy","scipy","matplotlib","basemap"],
-      test_suite = "tests.get_tests",
+      install_requires = requires,
+      #test_suite = "tests.get_tests",
      )
