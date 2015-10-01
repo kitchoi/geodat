8a63a14ff922a5a124b766a7779def3bad4bfd71
Kit Yan Choi <kit@kychoi.org>
2015-10-01 11:44:21 +0100

    Corrected the decorator that handles ImportError

--- tests/test_nc.py
+++ tests/test_nc.py
@@ -32,36 +32,54 @@ def var_dummy(ntime,nlat,nlon):
 
 
 
+def is_module_exist(module_name):
+    try:
+        importlib.import_module(module_name)
+    except ImportError:
+        return False
+    return True
+
+
 def expectImportErrorUnlessModuleExists(module_name):
     ''' Runtime dependencies will lead to ImportError 
     The library is supposed to check that and notify the 
     user that a dependency is required for a function
     '''
+    def new_test_func(func):
+        def new_func(testcase, *args, **kwargs):
+            try:
+                func(testcase, *args, **kwargs)
+            except ImportError as ierr:
+                print module_name+" cannot be imported and an ImportError "+\
+                    "is expected to be raised by the code.  The module name "+\
+                    "should be included in the error message"
+                testcase.assertRaisesRegexp(ierr, module_name)
+        return new_func
+
     try:
         importlib.import_module(module_name)
     except ImportError:
-        def _test_raise(testcase):
-            print module_name+" cannot be imported and an ImportError "+\
-                "is expected to be raised by the code.  The module name "+\
-                "should be included in the error message"
-            testcase.assertRaisesRegexp(ImportError, module_name)
-        return lambda func:_test_raise
+        return new_test_func
     return lambda func: func
 
 
 class NCVariableTestCase(unittest.TestCase):
     def setUp(self):
-        self.var = var_dummy(24,50,60)
         self.__name__ = "temp"
+        self.ntime = 24
+        self.nlat = 50
+        self.nlon = 60
+        self.var = var_dummy(self.ntime, self.nlat, self.nlon)
 
-    def test_getvar_data(self):
-        self.assertEqual(self.var.data.shape,(24,50,60))
-    
     def test_getCAxes(self):
-        self.assertEqual(self.var.getCAxes(),["T","Y","X"])
-    
+        self.assertEqual(self.var.getCAxes(), ["T", "Y", "X"])
+
+    def test_getAxis(self):
+        self.assertTrue(numpy.allclose(self.var.getAxis("Y"),
+                                       numpy.linspace(-90., 90., self.nlat)))
+
     def test_sliceVar(self):
-        self.assertEqual(self.var[:2,:3,:4].data.shape,(2,3,4))
+        self.assertEqual(self.var[:2, :3, :4].data.shape, (2, 3, 4))
 
     def test_getSlice(self):
         self.assertEqual(self.var.getSlice(latitude=(-45.,45.),
@@ -71,8 +89,8 @@ class NCVariableTestCase(unittest.TestCase):
                           slice(17, 34, None)))
 
     def test_getRegion(self):
-        self.assertEqual(self.var.getRegion(latitude=(-45.,45.),
-                                            longitude=(100.,200.)).data.shape,
+        self.assertEqual(self.var.getRegion(latitude=(-45., 45.),
+                                            longitude=(100., 200.)).data.shape,
                          (24, 24, 17))
 
     def test_getRegion_no_side_effect(self):
@@ -88,12 +106,34 @@ class NCVariableTestCase(unittest.TestCase):
         self.assertTrue((self.var.getDate("m",True) == months).\
                         all())
 
+    def test_getDate_monthly_unit(self):
+        time_dim = geodat.nc.Dimension(
+            data=numpy.arange(0, 24),
+            units="month since 0001-01-01",
+            dimname="time",
+            attributes={"calendar":"standard"})
+        month_iter = itertools.cycle(range(1,13))
+        dates = numpy.array([[imon/12+1, month_iter.next()]
+                             for imon in range(time_dim.data.size)])
+        self.assertTrue(numpy.allclose(time_dim.getDate("Ym"), dates))
+
     def test_wgtave(self):
         self.assertAlmostEqual(float(self.var.wgt_ave().data),
                                35999.5,1)
     
     def test_timeave(self):
-        self.assertEqual(self.var[...,:3,:4].time_ave().data.shape,(1,3,4))
+        self.assertEqual(self.var[..., :3, :4].time_ave().data.shape,(1, 3, 4))
+
+    def test_squeeze(self):
+        self.assertEqual(self.var[0, :3, :4].squeeze().data.shape, (3, 4))
+
+    def test_info(self):
+        try:
+            self.var.info();
+            self.var.info(True);
+        except Exception:
+            self.assertTrue(False, "Failed to print info")
+        self.assertTrue(True)
 
     def test_add_scalar(self):
         self.assertTrue(numpy.allclose((self.var + 2).data,
@@ -135,7 +175,6 @@ class NCVariableTestCase(unittest.TestCase):
         regridded = geodat.nc.pyferret_regrid(self.var[...,::2,::2], self.var)
         self.assertAlmostEqual(float(regridded.wgt_ave().data),
                                17999.0,1)
-
 
 if __name__== "__main__":
     unittest.main(verbosity=2)
