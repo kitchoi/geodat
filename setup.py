92ff7c18516d082d814ff851b1c70e01c2b09e25
Kit Yan Choi <kit@kychoi.org>
2015-10-01 14:53:05 +0100

    Added basemap to the requirement and added link in the setup command

--- setup.py
+++ setup.py
@@ -8,13 +8,15 @@ with open("requirements.txt") as f:
 requires = [ req.replace("\n","") for req in requires ]
 requires = [ req for req in requires if req ]
 
-
 setup(name='Geodat',
       version='1.0',
       description='Geophysical Data Analysis Tool (GeoDAT)',
       author='Kit Yan Choi',
       author_email='kit@kychoi.org',
       url='http://kychoi.org/geodat_doc/',
+      dependency_links = [
+          "http://sourceforge.net/projects/matplotlib/files/matplotlib-toolkits/"
+          ],
       packages=["geodat","geodat.plot"],
       install_requires = requires,
       test_suite = "tests.get_tests",
