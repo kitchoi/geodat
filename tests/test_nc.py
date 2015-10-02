b13ba865333ad716c04f13e051811268c2232053
Kit Yan Choi <kit@kychoi.org>
2015-10-02 10:26:20 +0100

    Made tests more descriptive

--- tests/test_nc.py
+++ tests/test_nc.py
@@ -6,9 +6,9 @@ import numpy
 import functools
 import itertools
 import urllib
+import logging
 
 import geodat.nc
-
 
 def var_dummy(ntime,nlat,nlon):
     month_day = 365.25/12.
@@ -46,34 +46,56 @@ def expectImportErrorUnlessModuleExists(module_name):
     The library is supposed to check that and notify the
     user that a dependency is required for a function
     '''
-    def new_test_func(func):
+    def new_test_func(test_func):
         def new_func(testcase, *args, **kwargs):
-            try:
-                func(testcase, *args, **kwargs)
-            except ImportError as ierr:
-                print module_name+" cannot be imported and an ImportError "+\
-                    "is expected to be raised by the code.  The module name "+\
-                    "should be included in the error message"
-                testcase.assertRaisesRegexp(ierr, module_name)
+            print module_name+" cannot be imported and an ImportError "+\
+                "is expected to be raised by the code.  The module name "+\
+                "should be included in the error message"
+            with testcase.assertRaisesRegexp(ImportError, module_name):
+                test_func(testcase, *args, **kwargs)
         return new_func
 
     try:
         importlib.import_module(module_name)
     except ImportError:
         return new_test_func
-    return lambda func: func
+    return lambda test_func: test_func
 
 
 class NCVariableTestCase(unittest.TestCase):
     def setUp(self):
-        " Create a geodat.nc.Variable for testing "
+        ''' Create a geodat.nc.Variable for testing '''
         self.ntime = 24
         self.nlat = 50
         self.nlon = 60
         self.var = var_dummy(self.ntime, self.nlat, self.nlon)
+        self.var_mean = self.var.data.mean()
+
+    def tearDown(self):
+        if os.path.exists("test_nc_file.nc"):
+            os.remove("test_nc_file.nc")
+
+    def test_var_setup(self):
+        ''' Test the other ways of creating Variable '''
+        # Inherit from a parent with new data
+        newvar = geodat.nc.Variable(data=numpy.ones_like(self.var.data),
+                                    parent=self.var)
+        self.assertEqual(newvar.varname, self.var.varname)
+        self.assertListEqual(newvar.dims, self.var.dims)
+        self.assertDictEqual(newvar.attributes, self.var.attributes)
+        # Inherit from a parent but update attributes
+        newvar = geodat.nc.Variable(data=numpy.ones_like(self.var.data),
+                                    parent=self.var,
+                                    attributes=dict(is_new="yes"))
+        self.assertEqual(newvar.varname, self.var.varname)
+        self.assertListEqual(newvar.dims, self.var.dims)
+        newvar.attributes.pop("is_new");
+        # attributes are only equal if the new attributes is removed
+        self.assertDictEqual(newvar.attributes, self.var.attributes)
+        
 
     def test_getCAxes(self):
-        " Test if the cartesian axes can be identified using the units "
+        ''' Test if the cartesian axes can be identified using the units '''
         self.assertEqual(self.var.getCAxes(), ["T", "Y", "X"])
 
     def test_getAxis(self):
@@ -90,7 +112,7 @@ class NCVariableTestCase(unittest.TestCase):
         self.assertEqual(len(self.var.getAxes()), 3)
 
     def test_sliceVar(self):
-        " geodat.nc.Variable can be sliced like a numpy array object "
+        '''Test if geodat.nc.Variable can be sliced like a numpy array object'''
         self.assertEqual(self.var[:2, :3, :4].data.shape, (2, 3, 4))
 
         sliced_var = self.var[2, :4, 4]
@@ -112,6 +134,7 @@ class NCVariableTestCase(unittest.TestCase):
                                        self.var.getLatitude()[:4]))
 
     def test_getSlice(self):
+        '''Test if slice objects can be created properly using lat-lon ranges'''
         self.assertEqual(self.var.getSlice(latitude=(-45.,45.),
                                            longitude=(100.,200.)),
                          (slice(None, None, None),
@@ -119,24 +142,28 @@ class NCVariableTestCase(unittest.TestCase):
                           slice(17, 34, None)))
 
     def test_getRegion(self):
+        '''Test if the sliced Variable has the right shape'''
         self.assertEqual(self.var.getRegion(latitude=(-45., 45.),
                                             longitude=(100., 200.)).data.shape,
                          (24, 24, 17))
 
     def test_getRegion_no_side_effect(self):
+        '''getRegion should not have side effect'''
         newvar = self.var.getRegion(latitude=(-45.,45.),
                                     longitude=(100.,200.))
         self.assertNotEqual(newvar.data.shape, self.var.data.shape)
 
     @expectImportErrorUnlessModuleExists("netCDF4")
     def test_getDate_month(self):
+        '''Is the correct month returned from getDate("m", True)'''
         months_iter = itertools.cycle(range(1,13))
         months = numpy.array([ months_iter.next()
                                for _ in range(self.var.data.shape[0])])
-        self.assertTrue((self.var.getDate("m",True) == months).\
-                        all())
+        self.assertTrue(numpy.allclose(self.var.getDate("m", True),
+                                       months))
 
     def test_getDate_monthly_unit(self):
+        '''Does getDate handle monthly unit properly'''
         time_dim = geodat.nc.Dimension(
             data=numpy.arange(0, 24),
             units="month since 0001-01-01",
@@ -148,8 +175,7 @@ class NCVariableTestCase(unittest.TestCase):
         self.assertTrue(numpy.allclose(time_dim.getDate("Ym"), dates))
 
     def test_wgtave(self):
-        self.assertAlmostEqual(float(self.var.wgt_ave().data),
-                               35999.5,1)
+        self.assertAlmostEqual(float(self.var.wgt_ave().data), self.var_mean, 1)
 
     def test_timeave(self):
         self.assertTrue(numpy.allclose(
@@ -160,11 +186,10 @@ class NCVariableTestCase(unittest.TestCase):
         self.assertEqual(self.var[0, :3, :4].squeeze().data.shape, (3, 4))
 
     def test_info(self):
-        try:
-            self.var.info();
-            self.var.info(True);
-        except Exception:
-            self.assertTrue(False, "Failed to print info")
+        stdnull = open(os.devnull, "w")
+        self.var.info(file_out=stdnull);
+        self.var.info(True, file_out=stdnull);
+        stdnull.close()
         self.assertTrue(True)
 
     def test_add_scalar(self):
@@ -194,8 +219,8 @@ class NCVariableTestCase(unittest.TestCase):
         self.assertTrue(numpy.allclose((self.var / 2).data,
                                        self.var.data / 2))
     def test_rdiv_scalar(self):
-        self.assertTrue(numpy.allclose((2 / self.var).data,
-                                       2 / self.var.data))
+        self.assertTrue(numpy.allclose((2. / (self.var + 1.)).data,
+                                       2. / (self.var.data + 1.)))
 
     def test_add_broadcast(self):
         self.assertTrue(numpy.allclose((self.var.time_ave() + self.var).data,
@@ -232,8 +257,8 @@ class NCVariableTestCase(unittest.TestCase):
         regional = self.var.getRegion(lat=(-40.,40.),lon=(100.,200.))
         conform_region = geodat.nc.conform_region(self.var, regional)
         regional_domain = regional.getDomain("XY")
-        self.assertTrue(conform_region["lat"] == regional_domain["Y"] and \
-                        conform_region["lon"] == regional_domain["X"])
+        self.assertTupleEqual(conform_region["lat"], regional_domain["Y"])
+        self.assertTupleEqual(conform_region["lon"], regional_domain["X"])
 
     @expectImportErrorUnlessModuleExists("pyferret")
     def test_conform_regrid(self):
@@ -250,7 +275,7 @@ class NCVariableTestCase(unittest.TestCase):
 
     @expectImportErrorUnlessModuleExists("netCDF4")
     def test_savefile(self):
-        geodat.nc.savefile("test_nc_file.nc",self.var)
+        geodat.nc.savefile("test_nc_file.nc", self.var)
         tmp = geodat.nc.getvar("test_nc_file.nc","temp")
         self.assertTrue(numpy.allclose(self.var.data,tmp.data))
         if os.path.exists("test_nc_file.nc"):
@@ -259,14 +284,12 @@ class NCVariableTestCase(unittest.TestCase):
     @expectImportErrorUnlessModuleExists("spharm")
     def test_regrid_spharm(self):
         regridded = geodat.nc.regrid(self.var,nlat=100,nlon=120)
-        self.assertAlmostEqual(float(regridded.wgt_ave().data),
-                               17999.5,1)
+        self.assertAlmostEqual(float(regridded.wgt_ave().data), self.var_mean, 1)
 
     @expectImportErrorUnlessModuleExists("pyferret")
     def test_regrid_pyferret(self):
         regridded = geodat.nc.pyferret_regrid(self.var[...,::2,::2], self.var)
-        self.assertAlmostEqual(float(regridded.wgt_ave().data),
-                               17999.0,1)
+        self.assertAlmostEqual(float(regridded.wgt_ave().data), self.var_mean, 1)
 
 if __name__== "__main__":
     unittest.main(verbosity=2)
