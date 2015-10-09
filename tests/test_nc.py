165847f781a2f08cc7f0c5af560a22496a2d71bd
Kit Yan Choi <kit@kychoi.org>
2015-10-09 12:26:09 +0100

    Added more tests

--- tests/test_nc.py
+++ tests/test_nc.py
@@ -6,7 +6,6 @@ import numpy
 import functools
 import itertools
 import urllib
-import logging
 
 import geodat.nc
 
@@ -71,10 +70,15 @@ class NCVariableTestCase(unittest.TestCase):
         self.nlon = 60
         self.var = var_dummy(self.ntime, self.nlat, self.nlon)
         self.var_mean = self.var.data.mean()
+        # Define files for IO testing
+        self.IO_FILENAMES = dict(TMP_FILENAME = "test_nc_tmp.nc",
+                                 TEST_DATA_NAME = "tests/data/test_nc_data.nc")
+
 
     def tearDown(self):
-        if os.path.exists("test_nc_file.nc"):
-            os.remove("test_nc_file.nc")
+        if os.path.exists(self.IO_FILENAMES["TMP_FILENAME"]):
+            os.remove(self.IO_FILENAMES["TMP_FILENAME"])
+
 
     def test_var_setup(self):
         ''' Test the other ways of creating a Variable '''
@@ -142,6 +146,7 @@ class NCVariableTestCase(unittest.TestCase):
         ''' Test if the cartesian axes can be identified using the units '''
         self.assertEqual(self.var.getCAxes(), ["T", "Y", "X"])
 
+
     def test_getAxis(self):
         " Test if the axis/axes can be retrieved properly "
         self.assertTrue(numpy.allclose(self.var.getAxis("Y"),
@@ -154,6 +159,7 @@ class NCVariableTestCase(unittest.TestCase):
         self.assertTrue(numpy.allclose(
             self.var.getAxis("X"), self.var.getLongitude()))
         self.assertEqual(len(self.var.getAxes()), 3)
+
 
     def test_sliceVar(self):
         '''Test if geodat.nc.Variable can be sliced like a numpy array object'''
@@ -191,43 +197,116 @@ class NCVariableTestCase(unittest.TestCase):
                                             longitude=(100., 200.)).data.shape,
                          (24, 24, 17))
 
+
     def test_getRegion_no_side_effect(self):
         '''getRegion should not have side effect'''
         newvar = self.var.getRegion(latitude=(-45.,45.),
                                     longitude=(100.,200.))
         self.assertNotEqual(newvar.data.shape, self.var.data.shape)
 
+
     @expectImportErrorUnlessModuleExists("netCDF4")
-    def test_getDate_month(self):
-        '''Is the correct month returned from getDate("m", True)'''
+    def test_getDate_special_calendar(self):
+        '''Test if getDate functions properly with special calendars
+        handled by netcdftime'''
         months_iter = itertools.cycle(range(1,13))
         months = numpy.array([ months_iter.next()
                                for _ in range(self.var.data.shape[0])])
         self.assertTrue(numpy.allclose(self.var.getDate("m", True),
                                        months))
 
-    def test_getDate_monthly_unit(self):
-        '''Does getDate handle monthly unit properly'''
-        time_dim = geodat.nc.Dimension(
+
+    def test_getDate(self):
+        '''Test if getDate functions properly using standard calendar'''
+        time_dim1 = geodat.nc.Dimension(
             data=numpy.arange(0, 24),
             units="month since 0001-01-01",
             dimname="time",
             attributes={"calendar":"standard"})
+        time_dim2 = geodat.nc.Dimension(
+            data=numpy.arange(0., 24.),
+            units="month since 0001-01-01 00:00:00",
+            dimname="time",
+            attributes={"calendar":"standard"})
+
         month_iter = itertools.cycle(range(1,13))
         dates = numpy.array([[imon/12+1, month_iter.next()]
-                             for imon in range(time_dim.data.size)])
-        self.assertTrue(numpy.allclose(time_dim.getDate("Ym"), dates))
+                             for imon in range(time_dim1.data.size)])
+
+        # Should work on different format of monthly unit
+        self.assertTrue(numpy.allclose(time_dim1.getDate("Ym"), dates))
+        self.assertTrue(numpy.allclose(time_dim2.getDate("Ym"), dates))
+
+        # Correct months by rolling forward
+        g = geodat.nc.create_monthly("standard",
+                                     "days since 0001-01-01","0001-01-01")
+        time_dim3 = geodat.nc.Dimension(data=numpy.array([g.next()
+                                                          for _ in range(24)]),
+                                        dimname="time",
+                                        units="days since 0001-01-01")
+        month_iter = itertools.cycle(range(1,13))
+        dates =  numpy.array([month_iter.next()
+                              for imon in range(time_dim1.data.size)])
+        self.assertTrue(numpy.allclose(time_dim3.getDate("m", True), dates))
+
+
+        # Correct months by rolling backward
+        time_dim3 = geodat.nc.Dimension(data=numpy.arange(30.,730.,30.),
+                                        dimname="time",
+                                        units="days since 0001-01-01")
+        month_iter = itertools.cycle(range(1,13))
+        dates =  numpy.array([month_iter.next()
+                              for imon in range(time_dim1.data.size)])
+        self.assertTrue(numpy.allclose(time_dim3.getDate("m", True), dates))
+
+        # Throw an error when the dimension is not a time axis
+        with self.assertRaisesRegexp(RuntimeError, "not a time axis"):
+            self.var.dims[1].getDate()
+
+        # Throw an error when asking for year but require non-duplicating months
+        with self.assertRaisesRegexp(RuntimeError, "applies when toggle=m"):
+            time_dim1.getDate("Y", True)
+
+        # Throw an error when toggle is not one of Y/m/d/H/M/S
+        with self.assertRaisesRegexp(ValueError, "toggle has to be one of"):
+            time_dim1.getDate("E")
+
+        # Throw an error when toggle is not iterable
+        with self.assertRaisesRegexp(TypeError, "toggle has to be iterable"):
+            time_dim1.getDate(0)
+
+        # Not actually a monthly data
+        time_dim2 = geodat.nc.Dimension(
+            data=numpy.arange(1, 380, 15),
+            units="days since 0001-01-01 00:00:00",
+            dimname="time",
+            attributes={"calendar":"standard"})
+        # This should just get a warning and run okay
+        print "Expect a warning:"
+        time_dim2.getDate("m")
+        # This will throw an error
+        with self.assertRaisesRegexp(RuntimeError, "not seem to be a monthly"):
+            time_dim2.getDate("m", True)
+
+        # Throw an error if the unit format is wrong
+        time_dim1.units = "month"
+        with self.assertRaisesRegexp(Exception, "should have a unit such as"):
+            time_dim1.getDate()
+
 
     def test_wgtave(self):
         self.assertAlmostEqual(float(self.var.wgt_ave().data), self.var_mean, 1)
+
 
     def test_timeave(self):
         self.assertTrue(numpy.allclose(
             self.var[..., :3, :4].time_ave().data.squeeze(),
             self.var.data[..., :3, :4].mean(axis=0)))
 
+
     def test_squeeze(self):
         self.assertEqual(self.var[0, :3, :4].squeeze().data.shape, (3, 4))
+
 
     def test_info(self):
         stdnull = open(os.devnull, "w")
@@ -235,6 +314,7 @@ class NCVariableTestCase(unittest.TestCase):
         self.var.info(True, file_out=stdnull);
         stdnull.close()
         self.assertTrue(True)
+
 
     def test_add_scalar(self):
         self.assertTrue(numpy.allclose((self.var + 2).data,
@@ -247,29 +327,38 @@ class NCVariableTestCase(unittest.TestCase):
     def test_sub_scalar(self):
         self.assertTrue(numpy.allclose((self.var - 2).data,
                                        self.var.data - 2))
+
+
     def test_rsub_scalar(self):
         self.assertTrue(numpy.allclose((2 - self.var).data,
                                        2 - self.var.data))
+
 
     def test_mul_scalar(self):
         self.assertTrue(numpy.allclose((self.var * 2).data,
                                        self.var.data * 2))
 
+
     def test_rmul_scalar(self):
         self.assertTrue(numpy.allclose((2 * self.var).data,
                                        self.var.data * 2))
 
+
     def test_div_scalar(self):
         self.assertTrue(numpy.allclose((self.var / 2).data,
                                        self.var.data / 2))
+
+
     def test_rdiv_scalar(self):
         self.assertTrue(numpy.allclose((2. / (self.var + 1.)).data,
                                        2. / (self.var.data + 1.)))
+
 
     def test_add_broadcast(self):
         self.assertTrue(numpy.allclose((self.var.time_ave() + self.var).data,
                                        self.var.data.mean(axis=0) + \
                                        self.var.data))
+
 
     @expectImportErrorUnlessModuleExists("netCDF4")
     def test_timeslices(self):
@@ -279,10 +368,12 @@ class NCVariableTestCase(unittest.TestCase):
                                               11., 2., "m", True).data.shape[0],
                          nNDJF)
 
+
     @expectImportErrorUnlessModuleExists("netCDF4")
     def test_climatology(self):
         clim = geodat.nc.climatology(self.var)
         self.assertTrue(clim.dims[clim.getCAxes().index("T")].is_climo())
+
 
     @expectImportErrorUnlessModuleExists("netCDF4")
     def test_anomaly(self):
@@ -292,10 +383,12 @@ class NCVariableTestCase(unittest.TestCase):
             (geodat.nc.clim2long(clim,anom) + anom).data,
             self.var.data))
 
+
     def test_concatenate(self):
         newvar = geodat.nc.concatenate([self.var[4], self.var[5]])
         self.assertTrue(numpy.allclose(newvar.getTime(),
                                        self.var.getTime()[4:6]))
+
 
     def test_conform_region(self):
         regional = self.var.getRegion(lat=(-40.,40.),lon=(100.,200.))
@@ -303,6 +396,7 @@ class NCVariableTestCase(unittest.TestCase):
         regional_domain = regional.getDomain("XY")
         self.assertTupleEqual(conform_region["lat"], regional_domain["Y"])
         self.assertTupleEqual(conform_region["lon"], regional_domain["X"])
+
 
     @expectImportErrorUnlessModuleExists("pyferret")
     def test_conform_regrid(self):
@@ -313,27 +407,61 @@ class NCVariableTestCase(unittest.TestCase):
                         numpy.allclose(conformed.getLongitude(),
                                        regioinal.getLongitude()))
 
+
     def test_is_monotonic(self):
         newvar = self.var.getRegion(lon=(100.,260.))
         self.assertTrue(newvar.dims[newvar.getCAxes().index("X")].is_monotonic())
 
+
     @expectImportErrorUnlessModuleExists("netCDF4")
     def test_savefile(self):
-        geodat.nc.savefile("test_nc_file.nc", self.var)
-        tmp = geodat.nc.getvar("test_nc_file.nc","temp")
-        self.assertTrue(numpy.allclose(self.var.data,tmp.data))
-        if os.path.exists("test_nc_file.nc"):
-            os.remove("test_nc_file.nc")
+        geodat.nc.savefile(self.IO_FILENAMES["TMP_FILENAME"], self.var)
+        if os.path.exists(self.IO_FILENAMES["TMP_FILENAME"]):
+            os.remove(self.IO_FILENAMES["TMP_FILENAME"])
+
+
+    def test_openfile(self):
+        ''' Test opening file and extracting variables from netCDF files'''
+        test_data_name = self.IO_FILENAMES["TEST_DATA_NAME"]
+
+        # Download data if it does not exist
+        if not os.path.exists(test_data_name):
+            # Create the test data directory if it does not exist
+            if not os.path.exists(os.path.dirname(test_data_name)):
+                os.makedirs(os.path.dirname(test_data_name))
+            # Download the data
+            urllib.urlretrieve("http://kychoi.org/geodat_test_data/sst_parts.nc",
+                               test_data_name)
+
+        # Check if a single variable can be extracted
+        var = geodat.nc.getvar(test_data_name, "sstMAM")
+        self.assertTupleEqual(var.data.shape, (3, 180, 360))
+
+        # Check if all of the variables can be extracted
+        dataset = geodat.nc.dataset(test_data_name)
+        self.assertTrue(all([varname in dataset.keys()
+                             for varname in ["sstSON", "sstMAM", "sstDJF",
+                                             "sstJJA"]]))
 
     @expectImportErrorUnlessModuleExists("spharm")
     def test_regrid_spharm(self):
         regridded = geodat.nc.regrid(self.var,nlat=100,nlon=120)
         self.assertAlmostEqual(float(regridded.wgt_ave().data), self.var_mean, 1)
 
+
     @expectImportErrorUnlessModuleExists("pyferret")
     def test_regrid_pyferret(self):
         regridded = geodat.nc.pyferret_regrid(self.var[...,::2,::2], self.var)
         self.assertAlmostEqual(float(regridded.wgt_ave().data), self.var_mean, 1)
+
+
+    def test_wgt_sum(self):
+        self.assertTrue(numpy.allclose(geodat.nc.wgt_sum(self.var).data,
+                                       numpy.array([[[1.62e+09]]]), rtol=0.01))
+        self.assertTrue(numpy.allclose(
+            geodat.nc.wgt_sum(self.var, axis=[1,2]).data[0],
+            numpy.array([[2805596.]]), rtol=0.01))
+
 
 if __name__== "__main__":
     unittest.main(verbosity=2)
