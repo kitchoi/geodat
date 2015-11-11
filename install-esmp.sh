88e647142eaaa0f5f6cbf255c9f06e2110f35eba
Kit Yan Choi <kit@kychoi.org>
2015-11-11 08:27:28 +0000

    gmake is make for Ubuntu

--- install-esmp.sh
+++ install-esmp.sh
@@ -5,11 +5,11 @@ tar -xf esmp.ESMF_6_3_0rp1_ESMP_01.tar
 cd esmp.ESMF_6_3_0rp1_ESMP_01/esmf
 export ESMF_DIR=$PWD
 export ESMF_INSTALL_PREFIX=$HOME/esmf
-gmake
-gmake install
+make
+make install
 
 ls $HOME/esmf/lib/lib0/*/esmf.mk
 
 cd ../ESMP
 export ESMFMKFILE=$HOME/esmf/lib/libO/Linux.gfortran.64.mpiuni.default/esmf.mk
-gmake build
+make build
