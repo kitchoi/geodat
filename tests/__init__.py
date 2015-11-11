124b807d263efe31993101e5c7cae02e93639079
Kit Yan Choi <kit@kychoi.org>
2015-11-11 18:08:45 +0000

    Updated full_suite tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,14 @@
 import os.path
 import unittest
 
+import test_nc
+import test_stat
+import test_keepdims
+import test_time_utils
+import test_monthly
 
 def full_suite():
-    import test_nc
-    import test_stat
-    import test_keepdims
-
     return unittest.TestSuite([unittest.TestLoader().loadTestsFromModule(module)
                                for module in [test_nc, test_stat,
-                                              test_keepdims]])
+                                              test_keepdims, test_time_utils,
+                                              test_monthly]])
